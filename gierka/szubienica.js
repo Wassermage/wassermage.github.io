67a2d31aa57b95815e259b02eddf108c1a6ca2d3
BuildTools <unconfigured@null.spigotmc.org>
2019-03-04 22:32:14 +0100

    Update szubienica.js

--- gierka/szubienica.js
+++ gierka/szubienica.js
@@ -1,9 +1,27 @@
-var haselka = new Array(5);
+var haselka = new Array(20);
 haselka[0] = "Bez pracy nie ma kołaczy";
 haselka[1] = "Gdzie dwóch się bije tam trzeci korzysta";
 haselka[2] = "Cicha woda brzegi rwie";
 haselka[3] = "Nadzieja matką głupich";
-haselka[4] = "Stół z powyłamywanymi nogami";
+//haselka[4] = "Stół z powyłamywanymi nogami";
+//haselka[4] = "Grzegorz Brzęczyszczykiewicz";
+haselka[4] = "Ziarnko do ziarnka a uzbiera się miarka";
+haselka[5] = "Apetyt rośnie w miarę jedzenia";
+haselka[6] = "Biednemu zawsze wiatr w oczy";
+haselka[7] = "Być pracowitym jak pszczoła";
+haselka[8] = "Co cię nie zabije to cię wzmocni";
+haselka[9] = "Co dwie głowy to nie jedna";
+haselka[10] = "Co się odwlecze to nie uciecze";
+haselka[11] = "Co za dużo to niezdrowo";
+haselka[12] = "Dla chcącego nic trudnego";
+haselka[13] = "Do wesela się zagoi";
+haselka[14] = "Dzieci i ryby głosu nie mają";
+haselka[15] = "Głupi ma zawsze szczęście";
+haselka[15] = "Jak Kuba Bogu tak Bóg Kubie";
+haselka[16] = "Jak sobie pościelesz, tak się wyśpisz";
+haselka[17] = "Każdy jest kowalem swego losu";
+haselka[18] = "Kłamstwo ma krótkie nogi";
+haselka[19] = "Mowa jest srebrem, a milczenie złotem";
 
 var haslo = haselka[Math.floor(Math.random() * haselka.length)];
 haslo = haslo.toUpperCase();
@@ -66,6 +84,8 @@ litery[34] = "Ź";
 
 function start() {
 	var tresc_diva = "";
+
+	document.getElementById("kategoria").innerHTML = "Kategoria: Przysłowia i powiedzenia";
 
 	for (i=0; i<=34; i++) {
 		var element = "lit" + i;
